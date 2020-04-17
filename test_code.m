2f3b9f17b73e3025164d43c34fbdac64d0c33a74
shassen <shassen@tamu.edu>
2020-04-16 20:06:20 -0500

    Added comments to the dataExtract file

--- test_code.m
+++ test_code.m
@@ -1,10 +1,9 @@
 clear all; clc; close all;
+% this is strictly just to test code that you want. Go crazy
+
 
 % load file
 mat_file = 'data_collection/walk2_100.mat';
 load(mat_file)
 
 [accel, gyro, mag_field, orientation, gps] = dataExtract(mat_file);
-
-
-
